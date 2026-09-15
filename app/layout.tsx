65d07ce7cb6f1540c953a17cccee6d5cb1dc95a4
Muhammad Hariz Lazuardi <harizlazuardi01@gmail.com>
2026-09-15 21:31:13 +0700

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
+      <head>
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+      </head>
       <body>
         <Providers>
           {children}
